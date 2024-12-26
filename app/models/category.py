4817fee5850b3e2a6452e398788ec2be6016c470
Dimitri Maksimov <dmitry-maksimov@yandex.ru>
2024-12-26 11:21:00 +0300

    update project

--- app/models/category.py
+++ app/models/category.py
@@ -1,15 +1,21 @@
 from app.backend.db import Base
-from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean
 from sqlalchemy.orm import relationship
+from app.models.products import Product
 
 
 class Category(Base):
     __tablename__ = 'categories'
-
+    __table_args__ = {'extend_existing': True}
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
     slug = Column(String, unique=True, index=True)
     is_active = Column(Boolean, default=True)
 
-# from sqlalchemy.schema import CreateTable
-# print(CreateTable(Category.__table__))
+    products = relationship("Product", back_populates="category")
+
+from sqlalchemy.schema import CreateTable
+print(CreateTable(Product.__table__))
+print(CreateTable(Category.__table__))
+
+
--- app/models/products.py
+++ app/models/products.py
@@ -13,8 +13,10 @@ class Product(Base):
     price = Column(Integer)
     image_url = Column(String)
     stock = Column(Integer)
+    category_id = Column(Integer, ForeignKey('categories.id'))
     rating = Column(Float)
     is_active = Column(Boolean, default=True)
+    category = relationship('Category', back_populates='products')
 
 # from sqlalchemy.schema import CreateTable
 # print(CreateTable(Product.__table__))
